431612da29d59fd02be4ad9dc52829ea9c1bff8e
Omer Hochman <omer.hochman@gmail.com>
2023-07-13 18:45:59 +0300

    Add avatar component

---
+++ apps/playground/pages/avatar/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="flex flex-col space-y-2 mt-10 justify-center items-center">
+    <k-avatar size="xs" />
+    <k-avatar
+      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+      size="sm"
+    />
+    <k-avatar
+      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+      variant="rounded"
+      size="md"
+    />
+    <k-avatar
+      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+      :notificationProps="{ color: 'gray', position: 'top-right' }"
+      size="lg"
+    />
+
+    <k-avatar
+      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+      :notificationProps="{ color: 'green', position: 'bottom-right' }"
+      size="xl"
+    />
+    <k-avatar
+      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+      variant="rounded"
+      :notificationProps="{ color: 'green', position: 'bottom-right' }"
+      size="2xl"
+    />
+    <k-avatar
+      :notificationProps="{ color: 'red', position: 'top-left' }"
+      size="xl"
+    />
+    <k-avatar placeholderInitials="JD" size="lg" />
+    <k-avatar
+      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+      size="md"
+      title="Tom Cook"
+      description="Software Engineer, Google"
+    />
+    <div class="isolate flex -space-x-2 overflow-hidden">
+      <k-avatar
+        class="rounded-full ring-2 ring-white"
+        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+        alt=""
+      />
+      <k-avatar
+        class="rounded-full ring-2 ring-white"
+        src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+        alt=""
+      />
+      <k-avatar
+        class="rounded-full ring-2 ring-white"
+        src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+        alt=""
+      />
+      <k-avatar
+        class="rounded-full ring-2 ring-white"
+        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
---
+++ packages/ui/src/runtime/components/avatar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="title || description" class="flex items-center group">
+    <BaseAvatar v-bind="$attrs"
+      ><template v-for="(_, slot) of $slots" v-slot:[slot]="scope"
+        ><slot :name="slot" v-bind="scope" /></template
+    ></BaseAvatar>
+    <div class="ml-3">
+      <p
+        class="text-sm font-medium text-brand-gray-700 group-hover:text-brand-gray-900"
+      >
+        {{ title }}
+      </p>
+      <p
+        class="text-xs font-medium text-brand-gray-500 group-hover:text-brand-gray-700"
+      >
+        {{ description }}
+      </p>
+    </div>
+  </div>
+  <BaseAvatar v-else v-bind="$attrs">
+    <template v-for="(_, slot) of $slots" v-slot:[slot]="scope"
+      ><slot :name="slot" v-bind="scope"
+    /></template>
+  </BaseAvatar>
+</template>
+
+<script setup lang="ts">
+import BaseAvatar from './avatar/base-avatar.vue'
+
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+})
+</script>
