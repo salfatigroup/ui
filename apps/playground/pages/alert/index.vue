475574d58b0dc4a3c028ae29c84ad30a0b1ecbfc
Omer Hochman <omer.hochman@gmail.com>
2023-07-15 01:13:16 +0300

    Add alert components

---
+++ apps/playground/pages/alert/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="flex flex-col space-y-2 mt-10 justify-center items-center">
+    <k-alert
+      title="Order completed"
+      variant="info"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            pariatur, ipsum similique veniam."
+      :buttons="[{ label: 'View Status' }, { label: 'Dismiss' }]"
+    />
+    <k-alert
+      title="Order completed"
+      variant="success"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            pariatur, ipsum similique veniam."
+      :buttons="[{ label: 'View Status' }, { label: 'Dismiss' }]"
+    />
+    <k-alert
+      title="Order completed"
+      variant="warn"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            pariatur, ipsum similique veniam."
+      :buttons="[{ label: 'View Status' }, { label: 'Dismiss' }]"
+    />
+    <k-alert
+      title="Order completed"
+      variant="danger"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            pariatur, ipsum similique veniam."
+      :buttons="[{ label: 'View Status' }, { label: 'Dismiss' }]"
+    />
+    <k-alert
+      title="Order completed"
+      variant="info"
+      accent
+      :onClose="() => console.log('close')"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            pariatur, ipsum similique veniam."
+    />
+    <k-alert
+      title="Order completed"
+      variant="success"
+      accent
+      :onClose="() => console.log('close')"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            pariatur, ipsum similique veniam."
+    />
+    <k-alert
+      title="Order completed"
+      variant="warn"
+      accent
+      :onClose="() => console.log('close')"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            pariatur, ipsum similique veniam."
+    />
+    <k-alert
+      title="Order completed"
+      variant="danger"
+      accent
+      :onLearnMore="() => console.log('close')"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            pariatur, ipsum similique veniam."
+    />
+  </div>
+</template>
