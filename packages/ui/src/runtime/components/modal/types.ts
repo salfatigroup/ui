176392036cf5da5a9d21ba74ecbaff3823f23d44
Omer Hochman <omer.hochman@gmail.com>
2023-07-15 17:59:03 +0300

    Add modal component

---
+++ packages/ui/src/runtime/components/modal/types.ts
@@ -0,0 +1,18 @@
+export type ModalProps = {
+  variant: 'info' | 'danger' | 'success' | 'warn'
+  layout: 'centered' | 'simple'
+  title: string
+  description: string
+  closeButton: boolean
+  open: boolean
+  onClose: () => void
+  buttons: ModalButtonProps[]
+  hideCloseButton: boolean
+}
+
+export type ModalButtonProps = {
+  label: string
+  onClick: () => void
+  variant: 'info' | 'secondary' | 'danger' | 'success' | 'warn'
+  [attr: string]: any
+}
