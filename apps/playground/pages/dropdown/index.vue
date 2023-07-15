abf04200014af2efdc56c69b10b85457780ffe0f
Elon Salfati <elon@salfati.group>
2023-07-15 20:16:28 +0300

    feat: improved dropdown

--- apps/playground/pages/dropdown/index.vue
+++ apps/playground/pages/dropdown/index.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="w-full flex flex-col space-y-2 mt-10">
     <k-dropdown :items="items" title="Menu"></k-dropdown>
+
+    <div>
+      <k-dropdown size="md" :items="items" prevent-chevron>
+        <IMoreVert class="w-5 h-5 text-brand-gray-500" variant="duotone" />
+
+        <template #item="{ item }">
+          <div class="flex items-center space-x-2">
+            <div class="flex-shrink-0 w-5 h-5 text-brand-gray-500">
+              <IMoreVert variant="duotone" />
+            </div>
+            <div class="text-brand-gray-700">{{ item.label }}</div>
+          </div>
+        </template>
+      </k-dropdown>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { IMoreVert } from '../../../../packages/ui/src/runtime/components/icon'
+
 const items = [
   {
     label: 'Option 1',
--- packages/ui/src/runtime/components/dropdown.vue
+++ packages/ui/src/runtime/components/dropdown.vue
@@ -2,12 +2,14 @@
   <Menu as="div" class="relative inline-block text-left">
     <div>
       <MenuButton
-        class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-brand-gray-900 shadow-sm ring-1 ring-inset ring-brand-gray-300 hover:bg-brand-gray-50"
+        class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white font-semibold text-brand-gray-900 shadow-sm ring-1 ring-inset ring-brand-gray-300 hover:bg-brand-gray-50"
+        :class="buttonSizeClasses"
       >
         <slot>
           {{ title }}
         </slot>
         <IChevronDown
+          v-if="!preventChevron"
           class="-mr-1 w-5 h-5 text-brand-gray-400"
           aria-hidden="true"
         />
@@ -32,7 +34,7 @@
             :key="item.value ?? item.label"
             @click="item.onClick?.()"
           >
-            <slot v-bind="menuItemSlot" :item="item">
+            <slot v-bind="menuItemSlot" :item="item" name="item">
               <div
                 :class="[
                   menuItemSlot.active
@@ -52,6 +54,7 @@
 </template>
 
 <script setup lang="ts">
+import { PropType, computed } from 'vue'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { IChevronDown } from './icon'
 
@@ -61,14 +64,39 @@ export type MenuItemType = {
   onClick?: () => void
 }
 
-const { title, items } = defineProps({
+export type Props = {
+  title?: string
+  items?: MenuItemType[]
+  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+  preventChevron: boolean
+}
+
+const props = defineProps({
   title: {
-    type: String,
+    type: String as PropType<Props['title']>,
     default: 'Options',
   },
   items: {
-    type: Array<MenuItemType>,
-    default: [],
+    type: Array as PropType<Props['items']>,
+    default: () => [] as Props['items'],
+  },
+  size: {
+    type: String as PropType<Props['size']>,
+    default: 'md',
+  },
+  preventChevron: {
+    type: Boolean as PropType<Props['preventChevron']>,
+    default: false,
   },
 })
+
+const buttonSizeClasses = computed(() => ({
+  'px-2 py-1': ['xs', 'sm'].includes(props.size),
+  'px-2.5 py-1.5': ['md'].includes(props.size),
+  'px-3 py-2': ['lg'].includes(props.size),
+  'px-3.5 py-2.5': ['xl'].includes(props.size),
+
+  'text-xs': ['xs'].includes(props.size),
+  'text-sm': ['sm', 'md', 'lg', 'xl'].includes(props.size),
+}))
 </script>
