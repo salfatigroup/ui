6dfdc35418a1668002d50eeaa81acd2b5fd74501
Omer Hochman <omer.hochman@gmail.com>
2023-07-19 16:49:25 +0300

    Support comment-input disabled mode

--- apps/playground/pages/comment-input/index.vue
+++ apps/playground/pages/comment-input/index.vue
@@ -5,6 +5,16 @@
         avatar="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
       />
     </ComponentCodeWrapper>
+    <ComponentCodeWrapper :code="example2">
+      <k-comment-input
+        disabled
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+      >
+        <template #customAddons>
+          <div class="text-sm text-brand-gray-600">Yesterday, 12:10 am</div>
+        </template>
+      </k-comment-input>
+    </ComponentCodeWrapper>
   </div>
 </template>
 
@@ -13,5 +23,15 @@ const example1 = `
 <k-comment-input
   avatar="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 />
+`
+const example2 = `
+<k-comment-input
+        disabled
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+      >
+  <template #customAddons>
+    <div class="text-sm text-brand-gray-600">Yesterday, 12:10 am</div>
+  </template>
+</k-comment-input>
 `
 </script>
--- packages/ui/src/runtime/components/comment-input.vue
+++ packages/ui/src/runtime/components/comment-input.vue
@@ -1,44 +1,46 @@
 <template>
   <div class="flex items-start space-x-4 w-full">
     <KAvatar :src="avatar" />
-    <div class="min-w-0 flex-1">
-      <form class="relative">
-        <div
-          class="overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-brand-gray-300 focus-within:ring-2 focus-within:ring-brand-600"
-        >
-          <label for="comment" class="sr-only">{{ placeholder }}</label>
-          <textarea
-            rows="3"
-            name="comment"
-            id="comment"
-            class="block w-full resize-none border-0 bg-transparent py-1.5 text-brand-gray-900 placeholder:text-brand-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-            :placeholder="placeholder"
-            @input="emit('update:modelValue', $event?.target?.value)"
-            :value="text"
-          />
+    <div class="min-w-0 flex-1 relative">
+      <div
+        class="overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-brand-gray-300 focus-within:ring-2 focus-within:ring-brand-600 min-h-fit"
+      >
+        <label for="comment" class="sr-only">{{ placeholder }}</label>
+        <textarea
+          v-bind="$attrs"
+          :disabled="disabled"
+          rows="3"
+          name="comment"
+          id="comment"
+          class="block w-full resize-none border-0 bg-transparent py-1.5 text-brand-gray-900 placeholder:text-brand-gray-400 focus:ring-0 sm:text-sm sm:leading-6 min-h-fit"
+          :placeholder="!disabled ? placeholder : ''"
+          @input="emit('update:modelValue', $event?.target?.value)"
+          :value="text"
+        />
 
-          <!-- Spacer element to match the height of the toolbar -->
-          <div class="py-2" aria-hidden="true">
-            <!-- Matches height of button in toolbar (1px border + 36px content height) -->
-            <div class="py-px">
-              <div class="h-9" />
-            </div>
+        <!-- Spacer element to match the height of the toolbar -->
+        <div class="py-2" aria-hidden="true">
+          <!-- Matches height of button in toolbar (1px border + 16px content height) -->
+          <div class="py-px">
+            <div class="h-4" />
           </div>
         </div>
+      </div>
 
-        <div
-          class="absolute inset-x-0 bottom-0 flex justify-between py-2 pl-3 pr-2"
-        >
-          <div class="flex items-center space-x-5">
-            <slot name="customAddons"></slot>
-          </div>
-          <div class="flex-shrink-0">
-            <slot name="actionButton">
-              <KButton @click="emit('submit')">{{ actionText }}</KButton>
-            </slot>
-          </div>
+      <div
+        class="absolute inset-x-0 bottom-0 flex justify-between pb-2 pl-3 pr-2 grow-0"
+      >
+        <div class="flex items-center space-x-5">
+          <slot name="customAddons"></slot>
         </div>
-      </form>
+        <div class="flex-grow-0">
+          <slot name="actionButton">
+            <KButton v-if="!disabled" @click="emit('post')">
+              {{ actionText }}
+            </KButton>
+          </slot>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -51,6 +53,7 @@ type Props = {
   avatar: string
   actionText: string
   text: string
+  disabled: boolean
 }
 
 defineProps({
@@ -70,10 +73,14 @@ defineProps({
     type: String as PropType<Props['text']>,
     default: '',
   },
+  disabled: {
+    type: Boolean as PropType<Props['disabled']>,
+    default: false,
+  },
 })
 
 const emit = defineEmits<{
   'update:modelValue': [value: string]
-  submit: []
+  post: []
 }>()
 </script>
