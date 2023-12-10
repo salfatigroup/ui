60f991c56e753837a26412b7d48083546c350e70
Omer Hochman <omer.hochman@gmail.com>
2023-12-10 15:18:57 +0100

    bump to 68 and revert pnpm lock

--- packages/ui/src/runtime/components/tags-input.vue
+++ packages/ui/src/runtime/components/tags-input.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <div
+  <div
     :class="[
       'flex shrink-0 items-center border-0 pl-1 pt-1 shadow-sm ring-inset rounded-md flex-wrap cursor-text',
       focused ? 'ring-brand-600 ring-2' : 'ring-brand-gray-300 ring-1',
@@ -32,68 +32,68 @@
         class="text-brand-gray-900 outline-none mr-1 mb-1"
       />
     </slot>
-  </div> -->
+  </div>
 </template>
 
 <script setup lang="ts">
-// import { PropType, computed, ref } from 'vue'
-// import Tag from './tag.vue'
+import { PropType, computed, ref } from 'vue'
+import Tag from './tag.vue'
 
-// const focused = ref(false)
-// const inputRef = ref<HTMLInputElement | null>(null)
+const focused = ref(false)
+const inputRef = ref<HTMLInputElement | null>(null)
 
-// type TagType = {
-//   index: number
-//   name: string
-//   attrs: Record<string, unknown>
-// }
-// type TagProps = {
-//   modelValue: TagType[]
-// }
+type TagType = {
+  index: number
+  name: string
+  attrs: Record<string, unknown>
+}
+type TagProps = {
+  modelValue: TagType[]
+}
 
-// const props = defineProps({
-//   modelValue: {
-//     type: Array as PropType<TagProps['modelValue']>,
-//     default: [],
-//   },
-// })
+const props = defineProps({
+  modelValue: {
+    type: Array as PropType<TagProps['modelValue']>,
+    default: [],
+  },
+})
 
-// const emit = defineEmits<{
-//   'update:modelValue': [value: TagType[]]
-// }>()
+const emit = defineEmits<{
+  'update:modelValue': [value: TagType[]]
+}>()
 
-// const innerModel = ref(props.modelValue)
+const innerModel = ref(props.modelValue)
 
-// const model = computed({
-//   get: () => innerModel.value,
-//   set: (value: TagType[]) => {
-//     emit('update:modelValue', value)
-//     innerModel.value = value
-//   },
-// })
+const model = computed({
+  get: () => innerModel.value,
+  set: (value: TagType[]) => {
+    emit('update:modelValue', value)
+    innerModel.value = value
+  },
+})
 
-// const input = ref('')
-// const addTag = () => {
-//   if (input.value.length > 0) {
-//     model.value.push({
-//       index: model.value.length,
-//       name: input.value,
-//       attrs: {},
-//     })
-//     model.value = [...model.value]
-//     input.value = ''
-//     setTimeout(() => {
-//       input.value = ''
-//     }, 0)
-//   }
-// }
+const input = ref('')
+const addTag = () => {
+  if (input.value.length > 0) {
+    model.value.push({
+      index: model.value.length,
+      name: input.value,
+      attrs: {},
+    })
+    model.value = [...model.value]
+    input.value = ''
+    setTimeout(() => {
+      input.value = ''
+    }, 0)
+  }
+}
 
-// const removeTag = (index?: number) => {
-//   if (index !== undefined) {
-//     model.value.splice(index, 1)
-//   } else if (input.value.length === 0 && model.value.length > 0) {
-//     model.value.pop()
-//   }
-//   model.value = [...model.value]
-// }
+const removeTag = (index?: number) => {
+  if (index !== undefined) {
+    model.value.splice(index, 1)
+  } else if (input.value.length === 0 && model.value.length > 0) {
+    model.value.pop()
+  }
+  model.value = [...model.value]
+}
 </script>
