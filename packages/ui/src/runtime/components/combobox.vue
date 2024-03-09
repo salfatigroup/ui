cd4d96cf9513870e8862e91c85c92638d0a6277f
Omer Hochman <omer.hochman@gmail.com>
2024-03-09 08:43:26 +0100

    fix combobox clear on  blur withtimeout v 86

--- packages/ui/src/runtime/components/combobox.vue
+++ packages/ui/src/runtime/components/combobox.vue
@@ -21,7 +21,7 @@
         <ComboboxInput
           class="w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-brand-gray-900 shadow-sm ring-1 ring-inset ring-brand-gray-300 focus:ring-2 focus:ring-inset focus:ring-brand-600 sm:text-sm sm:leading-6 disabled:cursor-not-allowed disabled:bg-brand-gray-50 disabled:text-brand-gray-500 disabled:ring-brand-gray-200 peer"
           @change="query = $event.target.value"
-          @blur="query = ''"
+          @blur="handleBlur"
           v-bind="$attrs"
           :disabled="disabled"
           :display-value="(option) => option?.label"
@@ -176,4 +176,10 @@ const comboboxOptionsClasses = computed(() => ({
   'right-full mr-1 top-0': props.direction === 'down-left',
   'right-full mr-1 bottom-0': props.direction === 'up-left',
 }))
+
+function handleBlur() {
+  setTimeout(() => {
+    query.value = ''
+  }, 100)
+}
 </script>
